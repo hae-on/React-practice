3aafbccc25fb96c23541d84c73baaa27675a2971
hae-on <solbi2004@naver.com>
2021-08-15 17:24:29 +0900

    [add]배열

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import "./App.css";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
   const name = "react";
@@ -13,6 +15,70 @@ function App() {
     fontSize: 24,
     padding: "1rem",
   };
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <>
       <Wrapper>
@@ -27,6 +93,14 @@ function App() {
       <Counter />
 
       <InputSample />
+
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
