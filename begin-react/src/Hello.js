b29c18b34fc6c208cdac01cb362865754237e502
hae-on <solbi2004@naver.com>
2021-08-16 19:38:37 +0900

    [add]클래스형 컴포넌트

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Hello({ color, name, isSpecial }) {
-  return (
-    <div style={{ color }}>
-      {/* 조건부 */}
-      {isSpecial && <b>*</b>}안녕하세요 {name}
-    </div>
-  );
+class Hello extends Component {
+  static defaultProps = {
+    name: "이름없음",
+  };
+  render() {
+    const { color, name, isSpecial } = this.props;
+
+    return (
+      <div style={{ color }}>
+        {/* 조건부 */}
+        {isSpecial && <b>*</b>}안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
 Hello.defaultProps = {
