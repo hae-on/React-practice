6d19b3cc95151159b9c7dc46cc14fabdb4249ee5
hae-on <solbi2004@naver.com>
2021-08-21 17:31:34 +0900

    habits state와 props을 이용한 배열 정리

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./app.css";
-import Habit from "./habit";
+import Habits from "./components/habits";
 
-function App() {
-  return <Habit />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    // habits : habits (둘이 다르지만 이름이 같을 경우 habits 하나만 작성 가능)
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
