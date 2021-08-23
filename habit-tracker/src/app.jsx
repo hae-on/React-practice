92497360aad3d7323a8402ffaebc0b6ac75f197f
hae-on <solbi2004@naver.com>
2021-08-23 21:04:27 +0900

    [add] habit

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./app.css";
+import Habit from "./components/habit";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  return <Habit />;
 }
 
 export default App;
