7d70696a720d7f314f19128a3ea5fa8195566b84
hae-on <solbi2004@naver.com>
2021-08-28 16:49:44 +0900

    todo list

--- todo-list/src/app.jsx
+++ todo-list/src/app.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import "./App.css";
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/todoCreate";
+import TodoHead from "./components/todoHead";
+import TodoList from "./components/todoList";
+import TodoTemplate from "./components/todoTemplate";
+import { TodoProvider } from "./todoContext";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
-  return <h1>Hello :)</h1>;
+  return (
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
+  );
 }
 
 export default App;
