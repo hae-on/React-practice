ecc4352397999bc4c017a06ea02e0a3825454803
hae-on <solbi2004@naver.com>
2021-09-10 10:20:42 +0900

    [add]card edit

--- card-maker/src/components/editor/editor.jsx
+++ card-maker/src/components/editor/editor.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import CardEditForm from "../card_edit_form.jsx/card_edit_form";
 import styles from "./editor.module.css";
 
-const Editor = () => (
+const Editor = ({ cards }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
+    {cards.map((card) => (
+      <CardEditForm card={card} />
+    ))}
   </section>
 );
 
