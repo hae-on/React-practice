d344baa51cb3e7ce4960ad94f57b186b64b5a869
hae-on <solbi2004@naver.com>
2023-06-16 14:02:39 +0900

    style: todo 스타일링

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
+import styles from './TodoList.module.css';
 
-const TodoList = () => {
+const TodoList = ({ filter }) => {
   const [todos, setTodos] = useState([
     {
       id: 123,
@@ -14,13 +17,40 @@ const TodoList = () => {
     },
   ]);
 
+  const handleTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+  };
+
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((todo) => todo.id !== deleted.id));
+  };
+
+  const filtered = getFilteredItems(todos, filter);
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-    </ul>
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleTodo} />
+    </section>
   );
 };
+
+function getFilteredItems(todos, filter) {
+  if (filter === 'all') return todos;
+  return todos.filter((todo) => todo.status === filter);
+}
 
 export default TodoList;
