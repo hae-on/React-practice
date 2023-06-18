dffd45977970430b03d8018bfae7190332c38fbf
hae-on <solbi2004@naver.com>
2023-06-18 18:04:08 +0900

    feat: 네트워크 통신 구현

--- youtube2/src/pages/Videos.jsx
+++ youtube2/src/pages/Videos.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../VideoCard';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 const Videos = () => {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
-  return <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 🥲</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default Videos;
